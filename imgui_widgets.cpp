Squamto/imgui_premake

RangeSelect/MultiSelect: Added ImGuiMultiSelectFlags_ClearOnClickWindowVoid. + Demo: showcase multiple selection scopes in same window.

imgui_widgets.cpp
@@ -6711,6 +6711,17 @@ ImGuiMultiSelectData* ImGui::EndMultiSelect()
     ImGuiMultiSelectState* state = &g.MultiSelectState;
     IM_ASSERT(g.MultiSelectState.FocusScopeId == g.CurrentFocusScopeId);
 
+    // Clear selection when clicking void?
+    // We specifically test for IsMouseDragPastThreshold(0) == false to allow box-selection!
+    if (g.MultiSelectFlags & ImGuiMultiSelectFlags_ClearOnClickWindowVoid)
+        if (IsWindowHovered() && g.HoveredId == 0)
+            if (IsMouseReleased(0) && IsMouseDragPastThreshold(0) == false && g.IO.KeyMods == ImGuiMod_None)
+            {
+                state->Out.RequestClear = true;
+                state->Out.RequestSelectAll = state->Out.RequestSetRange = false;
+            }
+
+    // Unwind
     if (g.MultiSelectFlags & ImGuiMultiSelectFlags_NoUnselect)
         state->Out.RangeValue = true;
     g.MultiSelectState.FocusScopeId = 0;

HDAKFD/imgui-sdl3-multiviewport

imgui_widgets.cpp
@@ -7071,6 +7071,17 @@ ImGuiMultiSelectData* ImGui::EndMultiSelect()
     ImGuiMultiSelectState* ms = &g.MultiSelectState;
     IM_ASSERT(g.MultiSelectState.FocusScopeId == g.CurrentFocusScopeId);
 
+    // Clear selection when clicking void?
+    // We specifically test for IsMouseDragPastThreshold(0) == false to allow box-selection!
+    if (g.MultiSelectFlags & ImGuiMultiSelectFlags_ClearOnClickWindowVoid)
+        if (IsWindowHovered() && g.HoveredId == 0)
+            if (IsMouseReleased(0) && IsMouseDragPastThreshold(0) == false && g.IO.KeyMods == ImGuiMod_None)
+            {
+                ms->Out.RequestClear = true;
+                ms->Out.RequestSelectAll = ms->Out.RequestSetRange = false;
+            }
+
+    // Unwind
     if (g.MultiSelectFlags & ImGuiMultiSelectFlags_NoUnselect)
         ms->Out.RangeValue = true;
     g.MultiSelectState.FocusScopeId = 0;